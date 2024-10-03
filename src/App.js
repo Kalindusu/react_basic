b02c65539c956f70f7a3e93c9b59c64532475c9d
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-04 01:49:50 +0530

    hook update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from './Components/Body';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Body/>
 {/* <BrowserRouter>
     <Routes>
     <Route path="/" element={<Header />}/>
---
+++ src/Components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Body = () => {
+  return (
+    <div>
+       <button onClick={()=>{console.log("clicked")}}>Click</button>
+    </div>
+  )
+}
+export default Body
+
