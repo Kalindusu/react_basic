b074a77461d44dac6c8619f0e55ff2fca780d943
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-06 14:58:57 +0530

    functional

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -16,4 +16,9 @@ console.log(jsxHeading);
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(jsxHeading)
 
+//functioal component
+const HeadingFunction=()=>{
+    return <h1 id="title">Hello, I am here</h1>
+}
+
 export default Body;
